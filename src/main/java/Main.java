7204feb698bf37d6804b6b7732ca5e61a3fdfdc1
sborisov88 <sborisov88@gmail.com>
2025-02-20 17:13:15 +0300

    отладка записи видео - заработало + рабочий docker compose файл

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,11 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.URL;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Map;
 
 public class Main {
     
@@ -16,17 +21,24 @@ public class Main {
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.setCapability("browserVersion", "132.0");
         chromeOptions.setCapability("platformName", "Linux");
-// Showing a test name instead of the session id in the Grid UI
         chromeOptions.setCapability("se:name", "My simple test");
-// Other type of metadata can be seen in the Grid UI by clicking on the
         chromeOptions.setCapability("se:sampleMetadata", "Sample metadata value");
+        chromeOptions.setCapability("se:recordVideo", true);
         WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444"), chromeOptions);
+        // Thread.sleep(2000);
         driver.get("http://www.google.com");
         driver.manage().window().maximize();
         // Using Actions class to simulate the Enter key press
         new Actions(driver).sendKeys("test").sendKeys(Keys.ENTER).perform();
-        Thread.sleep(500);
+        // Thread.sleep(2000);
+        String sessionId = ((RemoteWebDriver) driver).getSessionId().toString();
+        System.out.println("Session ID: " + sessionId);
         driver.quit();
+        // Download video from Selenoid
+        // String videoUrl = "http://localhost:4444/video/" + sessionId + ".mp4";
+        // try (InputStream in = new URL(videoUrl).openStream()) {
+        //     Files.copy(in, Paths.get("video_" + sessionId + ".mp4"), StandardCopyOption.REPLACE_EXISTING);
+        // }
 
     }
 
